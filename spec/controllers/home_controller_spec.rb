2991e0468c9b91425c651e758664aac366f1b6c9
Maarten van Gijssel <maarten@vgijssel.nl>
2014-07-24 15:11:55 +0200

    Updated test to reflect landingpage

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -6,10 +6,9 @@ describe HomeController do
   render_views
 
   describe "GET index" do
+    it "shows frontpage page when not signed in" do
       get :index
-
-    it "shows in-your-browser page when not signed in" do
-      expect(response.body).to match 'Your innovation only becomes social when it is shared!'
+      expect(response.body).to match 'Your innovation only becomes social when we work together!'
     end
   end
 end
