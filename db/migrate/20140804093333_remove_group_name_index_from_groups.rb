2f966b8002cbd76a6abfccdc2f38b60d4a9fbfbc
mvgijssel <maarten@factlink.com>
2014-08-04 14:00:08 +0200

    Removed unnecessary migration

--- db/migrate/20140804093333_remove_group_name_index_from_groups.rb
+++
@@ -1,9 +0,0 @@
-class RemoveGroupNameIndexFromGroups < ActiveRecord::Migration
-  def up
-    remove_index :groups,  column: :groupname
-  end
-
-  def down
-    add_index :groups, :groupname, {unique: true}
-  end
-end
